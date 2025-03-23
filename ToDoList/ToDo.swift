cdeb778bea2f9f586563a8565b7699dcbf4cf10d
daniel79438 <daniel79438@gmail.com>
2025-03-23 20:01:07 +0000

    Added SwiftData

--- ToDoList/ToDo.swift
+++ ToDoList/ToDo.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model
+class ToDo {
+    var item: String = ""
+    var reminderIsOn: Bool = false
+    var dueDate = Calendar.current.date(byAdding: .day, value: 1, to: Date.now)!
+    var notes: String = ""
+    var isCompleted: Bool = false
+
+    init(item: String = "", reminderIsOn: Bool = false, dueDate: Date = Date.now + 60*60*24, notes: String = "", isCompleted: Bool = false) {
+        self.item = item
+        self.reminderIsOn = reminderIsOn
+        self.dueDate = dueDate
+        self.notes = notes
+        self.isCompleted = isCompleted
+    }
+}
+
+

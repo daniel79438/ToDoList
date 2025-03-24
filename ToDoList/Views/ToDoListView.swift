bb326a4b5bce1d21b1d6de48e2ba6d862c8199ed
daniel79438 <daniel79438@gmail.com>
2025-03-24 12:59:55 +0000

    Sorting Orders Added

--- ToDoList/Views/ToDoListView.swift
+++ ToDoList/Views/ToDoListView.swift
@@ -8,71 +8,101 @@
 import SwiftUI
 import SwiftData
 
-struct ToDoListView: View {
+enum SortOption: String, CaseIterable {
+    case Unsorted = "Unsorted"
+    case alphabetical = "A-Z"
+    case chronoligcal = "Date"
+    case completed = "Not Done"
+}
+
+struct SortedToDoList: View {
     @Query var toDos: [ToDo] = []
-    @State private var sheetIsPresented = false
     @Environment(\.modelContext) var modelContext
+    let sortSelection: SortOption
+    
+    init(sortSelection: SortOption) {
+        self.sortSelection = sortSelection
+        switch self.sortSelection {
+        case .Unsorted:
+            _toDos = Query()
+        case .alphabetical:
+            _toDos = Query(sort: \.item, animation: .default)
+        case .chronoligcal:
+            _toDos = Query(sort: \.dueDate, animation: .default)
+        case .completed:
+            _toDos = Query(filter: #Predicate { $0.isCompleted == false })
+        }
+    }
     
     var body: some View {
-        NavigationStack{
-            List{
-                ForEach(toDos){ toDo in
-                    VStack(alignment: .leading){
-                           HStack {
-                            Image(systemName: toDo.isCompleted ? "checkmark.rectangle" : "rectangle")
-                                .onTapGesture {
-                                    toDo.isCompleted.toggle()
-                                    guard let _ = try? modelContext.save() else {
-                                        print("😡 ERROR: Save after .toggle on ToDoListView did not work")
-                                        return
-                                    }
-                                }
-                            
-                            NavigationLink {
-                                DetailView(toDo: toDo)
-                            } label: {
-                                Text(toDo.item)
-                            }
-                            
-                            .swipeActions {
-                                Button("Delete", role: .destructive) {
-                                    modelContext.delete(toDo)
-                                    guard let _ = try? modelContext.save() else {
-                                        print("😡 ERROR: Save after .delete on ToDoListView did not work")
-                                        return
-                                    }
+        List{
+            ForEach(toDos){ toDo in
+                VStack(alignment: .leading){
+                       HStack {
+                        Image(systemName: toDo.isCompleted ? "checkmark.rectangle" : "rectangle")
+                            .onTapGesture {
+                                toDo.isCompleted.toggle()
+                                guard let _ = try? modelContext.save() else {
+                                    print("😡 ERROR: Save after .toggle on ToDoListView did not work")
+                                    return
                                 }
                             }
-                            
-                        }
-                        .font(.title2)
                         
-                        HStack {
-                            Text(toDo.dueDate.formatted(date: .abbreviated, time: .shortened))
-                                .foregroundStyle(.secondary)
-                            
-                            if toDo.reminderIsOn {
-                                Image(systemName: "calendar.badge.clock")
-                                    .symbolRenderingMode(.multicolor)
+                        NavigationLink {
+                            DetailView(toDo: toDo)
+                        } label: {
+                            Text(toDo.item)
+                        }
+                        
+                        .swipeActions {
+                            Button("Delete", role: .destructive) {
+                                modelContext.delete(toDo)
+                                guard let _ = try? modelContext.save() else {
+                                    print("😡 ERROR: Save after .delete on ToDoListView did not work")
+                                    return
+                                }
                             }
+                        }
+                        
+                    }
+                    .font(.title2)
+                    
+                    HStack {
+                        Text(toDo.dueDate.formatted(date: .abbreviated, time: .shortened))
+                            .foregroundStyle(.secondary)
+                        
+                        if toDo.reminderIsOn {
+                            Image(systemName: "calendar.badge.clock")
+                                .symbolRenderingMode(.multicolor)
                         }
                     }
                 }
-                
-                // This is an alternative to .swipeActions:
-                // .onDelete Technique for using OUTSIDE a ForEach
-                
-                //                .onDelete { IndexSet in
-                //                    IndexSet.forEach({modelContext.delete(toDos[$0])})
-                //                    guard let _ = try? modelContext.save() else {
-                //                        print("😡 ERROR: Save after .delete on                                   ToDoListView did not work")
-                //                        return
-                //                    }
-                //                }
             }
+            
+            // This is an alternative to .swipeActions:
+            // .onDelete Technique for using OUTSIDE a ForEach
+            
+            //                .onDelete { IndexSet in
+            //                    IndexSet.forEach({modelContext.delete(toDos[$0])})
+            //                    guard let _ = try? modelContext.save() else {
+            //                        print("😡 ERROR: Save after .delete on                                   ToDoListView did not work")
+            //                        return
+            //                    }
+            //                }
+        }
+        .listStyle(.plain)
+    }
+}
+
+struct ToDoListView: View {
+    @State private var sheetIsPresented = false
+    @State private var sortSelection: SortOption = .Unsorted
+    
+    var body: some View {
+        NavigationStack{
+            SortedToDoList(sortSelection: sortSelection)
             .navigationTitle("To Do List")
             .navigationBarTitleDisplayMode(.automatic)
-            .listStyle(.plain)
             .sheet(isPresented: $sheetIsPresented) {
                 NavigationStack {
                     DetailView(toDo: ToDo())
@@ -86,6 +116,14 @@ struct ToDoListView: View {
                         Image(systemName: "plus")
                     }
                     
+                }
+                ToolbarItem(placement: .bottomBar) {
+                    Picker("", selection: $sortSelection) {
+                        ForEach(SortOption.allCases, id: \.self) { sortOrder in
+                            Text(sortOrder.rawValue)
+                        }
+                    }
+                    .pickerStyle(.segmented)
                 }
             }
         }
